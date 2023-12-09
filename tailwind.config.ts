da8b08d43311f72403c161eec1a60f62b56b3d91
shouta0715 <Myheroes0715@gmail.com>
2023-12-09 10:15:11 +0900

    setting vitest

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -74,4 +74,4 @@ module.exports = {
   },
   // eslint-disable-next-line global-require
   plugins: [require("tailwindcss-animate")],
-}
+};
---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { configDefaults, defineConfig } from "vitest/config";
+
+const alias = {
+  "@": `${__dirname}/src`,
+};
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./__tests__/setup.ts"],
+    include: ["./src/**/*.test.{ts,tsx}"],
+    exclude: [
+      ...configDefaults.exclude,
+      "src/**/e2e/**/*",
+      "*.config.{ts,js,tsx}",
+    ],
+  },
+  resolve: { alias },
+});
