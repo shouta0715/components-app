84045079abec35a1f3b4274988ecf2130909ef62
shouta0715 <Myheroes0715@gmail.com>
2023-12-16 21:00:58 +0900

    add: rdb test

--- src/services/components/get/index.ts
+++ src/services/components/get/index.ts
@@ -1,45 +1,46 @@
 import { Component } from "@prisma/client";
-import { cache } from "react";
+
 import { prisma } from "@/lib/client/prisma";
+import { NotFoundError } from "@/lib/errors";
 import { CompWithImgs } from "@/types/prisma";
 
-export const getComp = cache(async (id: string): Promise<Component | null> => {
+export const getComp = async (id: string): Promise<Component | null> => {
   const component = await prisma.component.findUnique({
     where: { id },
   });
 
+  if (!component) throw new NotFoundError();
+
   return component;
-});
+};
 
-export const getCompWithImgs = cache(
-  async (id: string): Promise<CompWithImgs | null> => {
-    const component = await prisma.component.findUnique({
-      where: { id },
-      include: {
-        previewImages: true,
+export const getCompWithImages = async (id: string): Promise<CompWithImgs> => {
+  const component = await prisma.component.findUnique({
+    where: { id },
+    include: {
+      previewImages: true,
+    },
+  });
+
+  if (!component) throw new NotFoundError();
+
+  return component;
+};
+
+export const getTopComps = async (take: number): Promise<CompWithImgs[]> => {
+  const components = await prisma.component.findMany({
+    take,
+    include: {
+      previewImages: {
+        take: 1,
       },
-    });
-
-    return component;
-  }
-);
-
-export const getTopComps = cache(
-  async (take: number): Promise<CompWithImgs[]> => {
-    const components = await prisma.component.findMany({
-      take,
-      include: {
-        previewImages: {
-          take: 1,
-        },
+    },
+    orderBy: {
+      likes: {
+        _count: "desc",
       },
-      orderBy: {
-        likes: {
-          _count: "desc",
-        },
-      },
-    });
+    },
+  });
 
-    return components;
-  }
-);
+  return components;
+};
