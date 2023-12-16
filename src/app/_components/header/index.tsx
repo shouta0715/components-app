1bf8bc72a2095db9ed1828dc91b6ddaae8df5679
shouta0715 <Myheroes0715@gmail.com>
2023-12-16 10:39:43 +0900

    add: top header

---
+++ src/app/_components/header/index.tsx
@@ -0,0 +1,38 @@
+import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export async function TopHeader() {
+  return (
+    <div className="mb-20 grid gap-4">
+      <div className=" grid max-w-md gap-4">
+        <h1 className="text-4xl font-bold  text-primary sm:text-7xl">
+          UI TRADE
+        </h1>
+        <p className="leading-7 text-muted-foreground sm:text-lg">
+          <span className="block">
+            コンポーネントやUIをシェアするプラットフォーム。
+          </span>
+          あなたが作成したUIを世界中の開発者と共有しましょう。
+          他の開発者が作成したUIを使って、デザインを勉強、開発を加速させましょう。
+          自由にシェアして、自由に使って、自由に開発。あなたのサイトをより良いものにしましょう。
+        </p>
+      </div>
+      <div className="flex flex-col gap-4 sm:flex-row">
+        <Button asChild>
+          <Link href="/docs">
+            Document
+            <ChevronRightIcon className="ml-2 hidden h-5 w-5 sm:inline-block" />
+          </Link>
+        </Button>
+        <Button asChild variant="outline">
+          <Link href="/components">
+            Popular Components
+            <ChevronRightIcon className="ml-2 hidden h-5 w-5 sm:inline-block" />
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
+import { TopHeader } from "@/app/_components/header";
 import { Categories } from "@/components/elements/category";
 import { getCategoriesByHome } from "@/services/category/get";
 
-export const fetchCache = "force-cache";
-export const revalidate = "false";
+export const revalidate = "86400";
 
 export default async function Home() {
   const categories = await getCategoriesByHome();
 
   return (
     <div>
+      <TopHeader />
       <Categories categories={categories} />
     </div>
   );
