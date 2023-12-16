d35a7720ed3703b261b5fcb2f1e6a57c1e2eee85
shouta0715 <Myheroes0715@gmail.com>
2023-12-16 21:00:12 +0900

    add: prisma-fabbrica

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,4 +1,10 @@
+import { PrismaClient } from "@prisma/client";
 import "@testing-library/jest-dom";
 import React from "react";
+import { initialize } from "@/tests/fabbrica";
+
+const prisma = new PrismaClient();
+
+initialize({ prisma });
 
 global.React = React;
