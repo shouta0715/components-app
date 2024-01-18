f2c8b19e21e1b33681cf0e8099e3eb5744b87b52
shouta0715 <Myheroes0715@gmail.com>
2024-01-18 10:33:39 +0900

    add: shallow route in navigate tabs

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import * as TabsPrimitive from "@radix-ui/react-tabs";
-import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 import * as React from "react";
 
 import { cn } from "@/utils";
+
+const NavigateParamContext = React.createContext<string>("tab");
 
 const Tabs = TabsPrimitive.Root;
 
@@ -39,42 +40,56 @@ const TabsTrigger = React.forwardRef<
 
 const NavigateTabs = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Root>
->(({ className, defaultValue, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Root> & {
+    params?: string;
+  }
+>(({ className, params = "tab", defaultValue, ...props }, ref) => {
   const searchParams = useSearchParams();
 
-  const defaultTab = searchParams.get("tab") ?? defaultValue;
+  const defaultTab = searchParams.get(params) ?? defaultValue;
 
   return (
-    <TabsPrimitive.Root
-      ref={ref}
-      className={className}
-      defaultValue={defaultTab}
-      {...props}
-    />
+    <NavigateParamContext.Provider value={params}>
+      <TabsPrimitive.Root
+        ref={ref}
+        className={className}
+        defaultValue={defaultTab}
+        {...props}
+      />
+    </NavigateParamContext.Provider>
   );
 });
 
 const NavigateTabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, value, children, ...props }, ref) => {
+>(({ className, value, children, onClick, ...props }, ref) => {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const params = React.useContext(NavigateParamContext);
 
   return (
     <TabsPrimitive.Trigger
       ref={ref}
-      asChild
       className={cn(
         "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
         className
       )}
+      onClick={(e) => {
+        const newParams = new URLSearchParams(searchParams.toString());
+        newParams.set(params, value);
+        window.history.replaceState(
+          null,
+          "",
+          `${pathname}?${newParams.toString()}`
+        );
+        onClick?.(e);
+      }}
       value={value}
       {...props}
+      aria-selected={searchParams.get(params) === value}
     >
-      <Link href={`${pathname}?tab=${value}`} replace>
-        {children}
-      </Link>
+      {children}
     </TabsPrimitive.Trigger>
   );
 });
