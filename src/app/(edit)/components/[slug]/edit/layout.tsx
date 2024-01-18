6be7cd72a200682c4820579060f9de33e8387028
shouta0715 <Myheroes0715@gmail.com>
2024-01-18 10:27:29 +0900

    fix: page and layout

--- src/app/(edit)/components/[slug]/edit/layout.tsx
+++ src/app/(edit)/components/[slug]/edit/layout.tsx
@@ -1,54 +1,10 @@
 import { Provider } from "jotai";
-import { notFound } from "next/navigation";
-import React, { Suspense } from "react";
-import { HydrateEditAtom } from "@/app/(edit)/components/[slug]/edit/_components/client/hydrate-atom";
-import { EditHeader } from "@/app/(edit)/components/[slug]/edit/_components/server/header";
-
-import { cacheGetCompWithFiles } from "@/app/(edit)/components/[slug]/edit/_hooks/cache";
-import {
-  EditStatus,
-  FinishedEditStatus,
-} from "@/app/(edit)/components/[slug]/edit/_hooks/contexts";
-import { getComponentCreatedStatus } from "@/app/(edit)/components/[slug]/edit/_hooks/utils";
-import { assertMine } from "@/lib/auth/handlers";
-import { Params } from "@/types/next";
-import { EditComp } from "@/types/prisma";
-
-function getInitialEditStatus(data: EditComp): EditStatus {
-  const { name, draft, files, document } = data;
-  if (!draft)
-    return {
-      ...FinishedEditStatus,
-      Summary: "EDITING",
-    };
-
-  const { summaryCreated, filesCreated, documentCreated } =
-    getComponentCreatedStatus({ name, files, document });
-
-  return {
-    Summary: summaryCreated ? "CREATED" : "EMPTY_EDITING",
-    Files: filesCreated ? "CREATED" : "EMPTY",
-    Document: documentCreated ? "CREATED" : "EMPTY",
-  };
-}
+import React from "react";
 
 export default async function Layout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-} & Params) {
-  const data = await cacheGetCompWithFiles(params.slug);
-  await assertMine(data.creatorId, notFound);
-
-  return (
-    <Provider>
-      <HydrateEditAtom initialEditStatus={getInitialEditStatus(data)}>
-        <Suspense>
-          <EditHeader />
-        </Suspense>
-        <div className="mt-10">{children}</div>
-      </HydrateEditAtom>
-    </Provider>
-  );
+}) {
+  return <Provider>{children}</Provider>;
 }
--- src/app/(edit)/components/[slug]/edit/page.tsx
+++ src/app/(edit)/components/[slug]/edit/page.tsx
@@ -1,6 +1,36 @@
-import { checkEditRedirect } from "@/app/(edit)/components/[slug]/edit/_hooks/hooks/redirect";
+import { notFound } from "next/navigation";
+import { HydrateEditAtom } from "@/app/(edit)/components/[slug]/edit/_components/client/hydrate-atom";
+import { EditDocument } from "@/app/(edit)/components/[slug]/edit/_components/server/document";
+import { EditFile } from "@/app/(edit)/components/[slug]/edit/_components/server/files";
+import { EditHeader } from "@/app/(edit)/components/[slug]/edit/_components/server/header";
+import { EditSummary } from "@/app/(edit)/components/[slug]/edit/_components/server/summary";
+import { cacheGetCompWithFiles } from "@/app/(edit)/components/[slug]/edit/_hooks/cache";
+import { NavigateTabs, TabsContent } from "@/components/ui/tabs";
+import { assertMine } from "@/lib/auth/handlers";
 import { Params } from "@/types/next";
 
 export default async function Page({ params }: Params) {
-  return checkEditRedirect(params.slug, "Home");
+  const data = await cacheGetCompWithFiles(params.slug);
+  await assertMine(data.creatorId, notFound);
+
+  return (
+    <HydrateEditAtom data={data}>
+      <NavigateTabs defaultValue="summary" params="section">
+        <EditHeader />
+
+        {/* Tab Contents */}
+        <TabsContent value="summary">
+          <EditSummary />
+        </TabsContent>
+
+        <TabsContent value="files">
+          <EditFile />
+        </TabsContent>
+
+        <TabsContent value="document">
+          <EditDocument />
+        </TabsContent>
+      </NavigateTabs>
+    </HydrateEditAtom>
+  );
 }
