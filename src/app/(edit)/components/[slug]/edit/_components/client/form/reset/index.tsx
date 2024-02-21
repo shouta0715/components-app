7ba51ac39aa69836ca0902198f8f811c78caa338
shouta0715 <Myheroes0715@gmail.com>
2024-02-21 23:32:16 +0900

    add reset alert dialog

--- src/app/(edit)/components/[slug]/edit/_components/client/form/reset/index.tsx
+++ src/app/(edit)/components/[slug]/edit/_components/client/form/reset/index.tsx
@@ -1,7 +1,54 @@
 "use client";
 
 import React from "react";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+
+function Alert({
+  onOpenChange,
+  open,
+  onConfirm,
+}: {
+  onOpenChange: (value: boolean) => void;
+  open: boolean;
+  onConfirm: () => void;
+}) {
+  return (
+    <AlertDialog onOpenChange={onOpenChange} open={open}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-base">
+            本当にリセットしますか？
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            リッセとした場合、入力した内容が失われます。
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>キャンセル</AlertDialogCancel>
+          <Button
+            className="font-bold"
+            onClick={() => {
+              onConfirm();
+              onOpenChange(false);
+            }}
+            variant="destructive"
+          >
+            リセットする
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
 
 export function ResetFormButton({
   onReset,
@@ -10,14 +57,19 @@ export function ResetFormButton({
   onReset: () => void;
   isDirty: boolean;
 }) {
+  const [alert, setAlert] = React.useState(false);
+
   return (
-    <Button
-      className="h-auto py-2 text-xs font-semibold transition-all"
-      disabled={!isDirty}
-      onClick={onReset}
-      variant="destructive"
-    >
-      入力をリセット
-    </Button>
+    <>
+      <Alert onConfirm={onReset} onOpenChange={setAlert} open={alert} />
+      <Button
+        className="h-auto py-2 text-xs font-semibold transition-all"
+        disabled={!isDirty}
+        onClick={() => setAlert(true)}
+        variant="destructive"
+      >
+        入力をリセット
+      </Button>
+    </>
   );
 }
