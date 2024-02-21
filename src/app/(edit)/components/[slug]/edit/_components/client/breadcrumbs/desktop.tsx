5acf213d46ed0044fedc8d7e75d8170900936386
shouta0715 <Myheroes0715@gmail.com>
2024-02-21 23:26:37 +0900

    fix edited redirect

--- src/app/(edit)/components/[slug]/edit/_components/client/breadcrumbs/desktop.tsx
+++ src/app/(edit)/components/[slug]/edit/_components/client/breadcrumbs/desktop.tsx
@@ -4,7 +4,6 @@
 import clsx from "clsx";
 import { useAtomValue } from "jotai";
 import { CheckCircle2, CircleDashed, Loader2, Pencil } from "lucide-react";
-import { useSearchParams } from "next/navigation";
 import React, { Suspense } from "react";
 import { DonNotSaveAlert } from "@/app/(edit)/components/[slug]/edit/_components/client/breadcrumbs/alert";
 import {
@@ -13,18 +12,12 @@ import {
   isEditingAtom,
   isPendingEditAtom,
 } from "@/app/(edit)/components/[slug]/edit/_hooks/contexts";
-import {
-  useRedirectSection,
-  useRedirectSectionHandler,
-} from "@/app/(edit)/components/[slug]/edit/_hooks/hooks/section";
+import { useRedirectSection } from "@/app/(edit)/components/[slug]/edit/_hooks/hooks/section";
 import {
   EditStatusValue,
   EditingSteps,
 } from "@/app/(edit)/components/[slug]/edit/_hooks/types";
-import {
-  getNextEditingStep,
-  paramsToEditingStep,
-} from "@/app/(edit)/components/[slug]/edit/_hooks/utils";
+
 import { NavigateTabsTrigger } from "@/components/ui/tabs";
 
 function Step({
@@ -32,92 +25,98 @@ function Step({
   isPending,
   status,
   isEditing,
+  openAlert,
   setOpenAlert,
 }: {
   name: EditingSteps;
   isPending: boolean;
   status: EditStatusValue;
   isEditing: boolean;
+  openAlert: boolean;
   setOpenAlert: (open: boolean) => void;
 }) {
   const { onRedirect, active } = useRedirectSection(name);
 
   return (
-    <NavigateTabsTrigger
-      aria-busy={isPending}
-      aria-controls={`tabs-${name}`}
-      aria-current={active ? "page" : undefined}
-      aria-disabled={isPending}
-      aria-label={name}
-      aria-selected={active}
-      className="group relative  min-w-0 flex-1 overflow-hidden rounded-none bg-background p-4 text-center text-sm font-medium capitalize text-muted-foreground focus:z-10 data-[state=active]:text-primary data-[state=active]:shadow-none"
-      disabled={isPending}
-      id={`tabs-${name}`}
-      onClick={() => {
-        if (isEditing) {
-          setOpenAlert(true);
-        } else {
-          onRedirect(name);
-        }
-      }}
-      role="tab"
-      value={name}
-    >
-      {status === "CREATED" ? (
-        <CheckCircle2 className="mr-2 size-6 fill-green-500 text-green-50" />
-      ) : status === "EDITING" ? (
-        <Pencil className="mr-2 size-6 text-primary" />
-      ) : status === "LOADING" ? (
-        <Loader2 className="mr-2 size-6 animate-spin text-primary" />
-      ) : status === "EMPTY" ? (
-        <CircleDashed className="mr-2 size-6 text-muted-foreground" />
-      ) : null}
-      {isPending || status === "LOADING" ? `Saving...` : name}
-      <span
-        aria-hidden="true"
-        className={clsx(
-          active ? "bg-destructive" : "bg-transparent",
-          "absolute inset-x-0 bottom-0 h-0.5"
-        )}
+    <>
+      <DonNotSaveAlert
+        onRedirect={() => onRedirect(name)}
+        open={openAlert}
+        setOpenAlert={setOpenAlert}
       />
-    </NavigateTabsTrigger>
+      <NavigateTabsTrigger
+        aria-busy={isPending}
+        aria-controls={`tabs-${name}`}
+        aria-current={active ? "page" : undefined}
+        aria-disabled={isPending}
+        aria-label={name}
+        aria-selected={active}
+        className="group relative  min-w-0 flex-1 overflow-hidden rounded-none bg-background p-4 text-center text-sm font-medium capitalize text-muted-foreground focus:z-10 data-[state=active]:text-primary data-[state=active]:shadow-none"
+        disabled={isPending}
+        id={`tabs-${name}`}
+        onClick={() => {
+          if (isEditing) {
+            setOpenAlert(true);
+          } else {
+            onRedirect(name);
+          }
+        }}
+        role="tab"
+        value={name}
+      >
+        {status === "CREATED" ? (
+          <CheckCircle2 className="mr-2 size-6 fill-green-500 text-green-50" />
+        ) : status === "EDITING" ? (
+          <Pencil className="mr-2 size-6 text-primary" />
+        ) : status === "LOADING" ? (
+          <Loader2 className="mr-2 size-6 animate-spin text-primary" />
+        ) : status === "EMPTY" ? (
+          <CircleDashed className="mr-2 size-6 text-muted-foreground" />
+        ) : null}
+        {isPending || status === "LOADING" ? `Saving...` : name}
+        <span
+          aria-hidden="true"
+          className={clsx(
+            active ? "bg-destructive" : "bg-transparent",
+            "absolute inset-x-0 bottom-0 h-0.5"
+          )}
+        />
+      </NavigateTabsTrigger>
+    </>
   );
 }
 
 function DesktopComponentBreadcrumbs() {
-  const [openAlert, setOpenAlert] = React.useState(false);
-  const { onRedirect } = useRedirectSectionHandler();
-  const searchParams = useSearchParams();
-  const currentSection = paramsToEditingStep(searchParams.get("section"));
-  const nextSection = getNextEditingStep(currentSection);
+  const [openAlertState, setOpenAlertState] = React.useState<{
+    [key in EditingSteps]: boolean;
+  }>({
+    summary: false,
+    files: false,
+    document: false,
+  });
+
   const values = useAtomValue(editStatusAtom);
   const isPending = useAtomValue(isPendingEditAtom);
   const isEditing = useAtomValue(isEditingAtom);
 
   return (
-    <>
-      <DonNotSaveAlert
-        onRedirect={() =>
-          onRedirect(nextSection === "preview" ? "document" : nextSection)
-        }
-        open={openAlert}
-        setOpenAlert={setOpenAlert}
-      />
-      <Suspense>
-        {editPaths.map(({ name }) => {
-          return (
-            <Step
-              key={name}
-              isEditing={isEditing}
-              isPending={isPending}
-              name={name}
-              setOpenAlert={setOpenAlert}
-              status={values[name].status}
-            />
-          );
-        })}
-      </Suspense>
-    </>
+    <Suspense>
+      {editPaths.map(({ name }) => {
+        return (
+          <Step
+            key={name}
+            isEditing={isEditing}
+            isPending={isPending}
+            name={name}
+            openAlert={openAlertState[name]}
+            setOpenAlert={(value) =>
+              setOpenAlertState((prev) => ({ ...prev, [name]: value }))
+            }
+            status={values[name].status}
+          />
+        );
+      })}
+    </Suspense>
   );
 }
 
--- src/app/(edit)/components/[slug]/edit/_components/client/breadcrumbs/mobile.tsx
+++ src/app/(edit)/components/[slug]/edit/_components/client/breadcrumbs/mobile.tsx
@@ -18,10 +18,7 @@ import {
   EditStatusValue,
   EditingSteps,
 } from "@/app/(edit)/components/[slug]/edit/_hooks/types";
-import {
-  getNextEditingStep,
-  paramsToEditingStep,
-} from "@/app/(edit)/components/[slug]/edit/_hooks/utils";
+import { paramsToEditingStep } from "@/app/(edit)/components/[slug]/edit/_hooks/utils";
 import {
   Select,
   SelectContent,
@@ -37,46 +34,64 @@ function SelectStep({
   isPending,
   status,
   active,
+  setOpenAlert,
+  openAlert,
 }: {
   name: EditingSteps;
   isPending: boolean;
   status: EditStatusValue;
   active: boolean;
+  setOpenAlert: (value: boolean) => void;
+  openAlert: boolean;
 }) {
+  const { onRedirect } = useRedirectSectionHandler();
+
   return (
-    <SelectItem className="w-full flex-1" disabled={isPending} value={name}>
-      <span className="flex items-center">
-        {status === "CREATED" ? (
-          <CheckCircle2 className="mr-2 size-6 fill-green-500 text-green-50" />
-        ) : status === "EDITING" ? (
-          <Pencil className="mr-2 size-6 text-primary" />
-        ) : status === "LOADING" ? (
-          <Loader2 className="mr-2 size-6 text-primary" />
-        ) : status === "EMPTY" ? (
-          <CircleDashed className="mr-2 size-6 text-muted-foreground" />
-        ) : null}
-        <span
-          className={clsx(
-            "capitalize",
-            active ? " font-semibold text-primary" : "text-muted-foreground"
-          )}
-        >
-          {isPending || status === "LOADING" ? `Saving...` : name}
+    <>
+      <DonNotSaveAlert
+        onRedirect={() => onRedirect(name)}
+        open={openAlert}
+        setOpenAlert={setOpenAlert}
+      />
+      <SelectItem className="w-full flex-1" disabled={isPending} value={name}>
+        <span className="flex items-center">
+          {status === "CREATED" ? (
+            <CheckCircle2 className="mr-2 size-6 fill-green-500 text-green-50" />
+          ) : status === "EDITING" ? (
+            <Pencil className="mr-2 size-6 text-primary" />
+          ) : status === "LOADING" ? (
+            <Loader2 className="mr-2 size-6 text-primary" />
+          ) : status === "EMPTY" ? (
+            <CircleDashed className="mr-2 size-6 text-muted-foreground" />
+          ) : null}
+          <span
+            className={clsx(
+              "capitalize",
+              active ? " font-semibold text-primary" : "text-muted-foreground"
+            )}
+          >
+            {isPending || status === "LOADING" ? `Saving...` : name}
+          </span>
         </span>
-      </span>
-    </SelectItem>
+      </SelectItem>
+    </>
   );
 }
 
 function MobileComponentBreadcrumbs() {
   // Alert
-  const [openAlert, setOpenAlert] = React.useState(false);
+  const [openAlertState, setOpenAlertState] = React.useState<{
+    [key in EditingSteps]: boolean;
+  }>({
+    summary: false,
+    files: false,
+    document: false,
+  });
 
   // sections
   const { onRedirect } = useRedirectSectionHandler();
   const searchParams = useSearchParams();
   const currentSection = paramsToEditingStep(searchParams.get("section"));
-  const nextSection = getNextEditingStep(currentSection);
 
   // atoms
   const values = useAtomValue(editStatusAtom);
@@ -84,49 +99,45 @@ function MobileComponentBreadcrumbs() {
   const isEditing = useAtomValue(isEditingAtom);
 
   return (
-    <>
-      <DonNotSaveAlert
-        onRedirect={() =>
-          onRedirect(nextSection === "preview" ? "document" : nextSection)
+    <Select
+      defaultValue={currentSection}
+      onValueChange={(value: EditingSteps) => {
+        if (isEditing) {
+          setOpenAlertState((prev) => ({ ...prev, [value]: true }));
+        } else {
+          onRedirect(value);
         }
-        open={openAlert}
-        setOpenAlert={setOpenAlert}
-      />
-      <Select
-        defaultValue={currentSection}
-        onValueChange={(value: EditingSteps) => {
-          if (isEditing) {
-            setOpenAlert(true);
-          } else {
-            onRedirect(value);
-          }
-        }}
-      >
-        <SelectTrigger className="flex rounded-none border-none capitalize focus:ring-0 [&_span]:flex-1">
-          <SelectValue
-            className="flex flex-1 text-primary"
-            placeholder="選択してください"
-          />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>編集する内容を選択してください</SelectLabel>
+      }}
+      value={currentSection}
+    >
+      <SelectTrigger className="flex rounded-none border-none capitalize focus:ring-0 [&_span]:flex-1">
+        <SelectValue
+          className="flex flex-1 text-primary"
+          placeholder="選択してください"
+        />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>編集する内容を選択してください</SelectLabel>
 
-            {editPaths.map(({ name }) => {
-              return (
-                <SelectStep
-                  key={name}
-                  active={currentSection === name}
-                  isPending={isPending}
-                  name={name}
-                  status={values[name].status}
-                />
-              );
-            })}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-    </>
+          {editPaths.map(({ name }) => {
+            return (
+              <SelectStep
+                key={name}
+                active={currentSection === name}
+                isPending={isPending}
+                name={name}
+                openAlert={openAlertState[name]}
+                setOpenAlert={(value) => {
+                  setOpenAlertState((prev) => ({ ...prev, [name]: value }));
+                }}
+                status={values[name].status}
+              />
+            );
+          })}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 }
 
