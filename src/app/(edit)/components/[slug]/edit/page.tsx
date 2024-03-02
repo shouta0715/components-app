23759b52a909593a43ad7833f9bb9f9ccf233899
shouta0715 <Myheroes0715@gmail.com>
2024-03-02 17:10:10 +0900

    add: document props

--- src/app/(edit)/components/[slug]/edit/page.tsx
+++ src/app/(edit)/components/[slug]/edit/page.tsx
@@ -10,6 +10,7 @@ import { EditSummary } from "@/app/(edit)/components/[slug]/edit/_features/summa
 
 import { TabsContent, TabsList } from "@/components/ui/tabs";
 import { assertMine } from "@/lib/auth/handlers";
+
 import { EditFilesInput, PreviewType } from "@/lib/schema/client/edit/files";
 import { EditSummaryInput } from "@/lib/schema/client/edit/summary";
 import { Params } from "@/types/next";
@@ -18,7 +19,14 @@ export default async function Page({ params }: Params) {
   const data = await cacheGetCompWithFiles(params.slug);
   await assertMine(data.creatorId, notFound);
 
-  const { name, description, categoryName, previewUrl, functionName } = data;
+  const {
+    name,
+    description,
+    categoryName,
+    previewUrl,
+    functionName,
+    document,
+  } = data;
 
   const summaryDefaultValues: EditSummaryInput = {
     name,
@@ -71,7 +79,7 @@ export default async function Page({ params }: Params) {
               <EditFile defaultValues={filesDefaultValues} draft={data.draft} />
             </TabsContent>
             <TabsContent value="document">
-              <EditDocument />
+              <EditDocument defaultValues={document} draft={data.draft} />
             </TabsContent>
           </div>
         </TabsList>
