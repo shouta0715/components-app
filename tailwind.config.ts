ca53515400044e68fa96e0f64a33c81bb814fc40
shouta0715 <Myheroes0715@gmail.com>
2024-03-03 21:40:21 +0900

    add: animation and success color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         code: "#22272e",
         foreground: "hsl(var(--foreground))",
+        success: "hsl(var(--success))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
@@ -76,10 +77,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "fade-in": {
+          "0%": { transform: "translateY(-10px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in": "fade-in 0.25s ease-out forwards",
       },
     },
   },
