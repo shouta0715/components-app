bf4deb15031fbdcb5b76016663810e170ccab393
shouta0715 <Myheroes0715@gmail.com>
2024-03-08 09:52:14 +0900

    add: run prisma and handler prisma error

--- src/lib/client/prisma.ts
+++ src/lib/client/prisma.ts
@@ -1,4 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import {
+  PrismaClientInitializationError,
+  PrismaClientKnownRequestError,
+  PrismaClientUnknownRequestError,
+  PrismaClientValidationError,
+} from "@prisma/client/runtime/library";
+import { BadRequestError, InternalServerError } from "@/lib/errors";
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -11,3 +18,28 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export function handlePrismaError(err: unknown): never {
+  if (err instanceof PrismaClientValidationError) {
+    throw new BadRequestError();
+  }
+  if (err instanceof PrismaClientKnownRequestError) {
+    throw new BadRequestError();
+  }
+  if (err instanceof PrismaClientUnknownRequestError) {
+    throw new BadRequestError();
+  }
+  if (err instanceof PrismaClientInitializationError) {
+    throw new InternalServerError();
+  }
+  throw err;
+}
+
+// eslint-disable-next-line consistent-return
+export const runPrisma = async <T>(fn: () => Promise<T>): Promise<T> => {
+  try {
+    return await fn();
+  } catch (err) {
+    handlePrismaError(err);
+  }
+};
