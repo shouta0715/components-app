70a4824d3c6e1bfe8cd886c39a09c273185b7a93
shouta0715 <Myheroes0715@gmail.com>
2024-03-11 22:19:07 +0900

    add: build time

--- src/app/(app)/(page)/page.tsx
+++ src/app/(app)/(page)/page.tsx
@@ -1,12 +1,16 @@
 import FileTypeComponents from "@/app/(app)/(page)/_components/file-type-component";
 import { TopHeader } from "@/app/(app)/(page)/_components/header";
 import { TrendComponents } from "@/app/(app)/(page)/_components/trend";
+import { BuildTimeBadge } from "@/components/elements/badges/build-time";
 import { Categories } from "@/components/elements/category";
 import { Section } from "@/components/ui/section";
 import { getPopularCategories } from "@/services/category/get/popular";
 import { getTrendComponents } from "@/services/components/get/trend";
+import { getBuildDate } from "@/utils/time/build-date";
 
 export const dynamic = "error";
+
+const buildDate = getBuildDate();
 
 export default async function Home() {
   const [categories, components] = await Promise.all([
@@ -22,19 +26,11 @@ export default async function Home() {
       <TopHeader />
       <Section.Root>
         <Section>
-          <Section.Title>Trend Components</Section.Title>
+          <Section.Title className="flex items-center justify-between">
+            Trend Components <BuildTimeBadge date={buildDate} />
+          </Section.Title>
           <Section.Content>
             <TrendComponents components={components} />
-          </Section.Content>
-          <Section.MoreLink href="/components/trend">
-            トレンドをもっと見る
-          </Section.MoreLink>
-        </Section>
-
-        <Section>
-          <Section.Title>Files Types</Section.Title>
-          <Section.Content>
-            <FileTypeComponents />
           </Section.Content>
         </Section>
 
@@ -46,6 +42,13 @@ export default async function Home() {
           <Section.MoreLink href="/categories">
             カテゴリーもっと見る
           </Section.MoreLink>
+        </Section>
+
+        <Section>
+          <Section.Title>Files Types</Section.Title>
+          <Section.Content>
+            <FileTypeComponents />
+          </Section.Content>
         </Section>
       </Section.Root>
     </div>
