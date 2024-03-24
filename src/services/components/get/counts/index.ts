5acb0d3a5d680c178b5b6175a48763ef8a2d3a8a
shouta0715 <Myheroes0715@gmail.com>
2024-03-24 18:46:57 +0900

    fix: seed and type

--- src/services/components/get/counts/index.ts
+++
@@ -1,5 +0,0 @@
-import { prisma, runPrisma } from "@/lib/client/prisma";
-
-export const getComponentCount = async (): Promise<number> => {
-  return runPrisma(() => prisma.component.count());
-};
--- src/types/file/index.ts
+++ src/types/file/index.ts
@@ -1,8 +1,7 @@
-import { $Enums } from "@prisma/client";
-import { picklist } from "valibot";
-
-export type Extension = $Enums.Extension;
+import { Input, picklist } from "valibot";
 
 export const extensions = picklist(["tsx", "html", "css", "js", "ts", "jsx"]);
+
+export type Extension = Input<typeof extensions>;
 
 export const imageExtensions = picklist(["png", "jpg", "jpeg", "gif", "webp"]);
