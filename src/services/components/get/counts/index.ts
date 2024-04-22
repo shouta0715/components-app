431ee3cd922ac61b0d1091281d468cc7cb471df7
shouta0715 <Myheroes0715@gmail.com>
2024-04-22 16:26:31 +0900

    add: get liked count

--- src/services/components/get/counts/index.ts
+++ src/services/components/get/counts/index.ts
@@ -3,3 +3,13 @@ import { prisma, runPrisma } from "@/lib/client/prisma";
 export const getComponentCount = async (): Promise<number> => {
   return runPrisma(() => prisma.component.count());
 };
+
+export const getLikesComponentCount = async (id: string): Promise<number> => {
+  return runPrisma(() =>
+    prisma.like.count({
+      where: {
+        userId: id,
+      },
+    })
+  );
+};
